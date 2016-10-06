5f7628fd8eb014493f949bff8f80d5d5a76aa1e0
Julien Church <julienchurch@gmail.com>
2016-10-06 04:41:47 -0700

    Exception overhaul, API upgrade
    Not being able to return a default image isn't catastrophic and is
    probably best handled by the consumer. As such, the FileNotFound
    exception is caught and but logged.
    
    Other similar changes have been made elsewhere in the code, as well as
    providing enhanced error messages for when things go wrong (e.g.,
    trying to upload a missing file).
    
    API now allows for the deletion of files and the swapping out of
    default file based on file name.

--- flask_ffs/ffs_mixin.py
+++ flask_ffs/ffs_mixin.py
@@ -26,6 +26,11 @@ class FFSMixin(object):
   def _starts_with_default_prefix(self, filename):
     return filename.find(self.default_prefix) == 0
 
+  def _add_default_prefix(self, filename):
+    if self._starts_with_default_prefix(filename):
+      return filename
+    return self.default_prefix + filename
+
   def _strip_default_prefix(self, filename):
     if self._starts_with_default_prefix(filename):
       return filename.replace(self.default_prefix, '', 1)
@@ -37,30 +42,30 @@ class FFSMixin(object):
     base_with_rand_str = '.'.join([base, uuid_str])
     return ''.join([base_with_rand_str, ext])
 
-  def _add_default_prefix(self, filename):
-    if self._starts_with_default_prefix(filename):
-      return filename
-    return self.default_prefix + filename
-
-  def _purge_defaults(self):
-    filenames = os.listdir(self.images_dir)
-    for filename in filenames:
-      src = os.path.join(self.images_dir, filename)
-      dst = os.path.join(self.images_dir, self._strip_default_prefix(filename))
-      os.rename(src, dst)
-
   def _get_default_filename(self):
-    filenames = os.listdir(self.images_dir)
-    for filename in filenames:
-      if self._starts_with_default_prefix(filename):
-        return filename
-    raise Exception('No default image has been set for this object.')
+    try:
+      filenames = os.listdir(self.images_dir)
+      for filename in filenames:
+        if self._starts_with_default_prefix(filename):
+          return filename
+    except FileNotFoundError as e:
+      print(e)
+      print('The directory you\'re trying to access doesn\'t exist.')
+      print('This is most likely because you haven\'t saved any images to')
+      print('your model yet, which is when the directory is created.\n')
 
   def _get_supplementary_filenames(self):
     filenames = os.listdir(self.images_dir)
     for filename in filenames:
       if not self._starts_with_default_prefix(filename):
         yield filename
+
+  def __purge_defaults(self):
+    filenames = os.listdir(self.images_dir)
+    for filename in filenames:
+      src = os.path.join(self.images_dir, filename)
+      dst = os.path.join(self.images_dir, self._strip_default_prefix(filename))
+      os.rename(src, dst)
 
   def has_valid_ext(self, filename):
     _, ext = os.path.splitext(filename)
@@ -70,22 +75,47 @@ class FFSMixin(object):
     return self._add_uuid(secure_filename(filename))
 
   def save_default_image(self, image):
-    if self.has_valid_ext(image.filename):
-      unique_filename = self.make_unique_filename(image.filename)
-      filename = self._add_default_prefix(unique_filename)
-      self._make_image_dir_if_not_exists()
-      self._purge_defaults()
-      save_path = os.path.join(self.images_dir, filename)
-      image.save(save_path)
-    else: raise
+    if not image:
+      raise Exception('You haven\'t provided an image to save.')
+    if not self.has_valid_ext(image.filename):
+      raise Exception('Invalid or missing file type: {0}.'.format(image.filename))
+    unique_filename = self.make_unique_filename(image.filename)
+    filename = self._add_default_prefix(unique_filename)
+    self._make_image_dir_if_not_exists()
+    self.__purge_defaults()
+    save_path = os.path.join(self.images_dir, filename)
+    image.save(save_path)
 
   def save_supplementary_image(self, image):
-    if self.has_valid_ext(image.filename):
-      filename = self.make_unique_filename(image.filename)
-      self._make_image_dir_if_not_exists()
-      save_path = os.path.join(self.images_dir, filename)
-      image.save(save_path)
-    else: raise
+    if not image:
+      raise Exception('You haven\'t provided an image to save.')
+    if not self.has_valid_ext(image.filename):
+      raise Exception('Invalid or missing file type: {0}.'.format(image.filename))
+    filename = self.make_unique_filename(image.filename)
+    self._make_image_dir_if_not_exists()
+    save_path = os.path.join(self.images_dir, filename)
+    image.save(save_path)
+
+  def swap_default_image_to(self, target):
+    filenames = os.listdir(self.images_dir)
+    for filename in filenames:
+      if filename == target:
+        self.__purge_defaults()
+        src = os.path.join(self.images_dir, filename)
+        dst = os.path.join(self.images_dir, self._add_default_prefix(filename))
+        os.rename(src, dst)
+        return
+      if filename == self._add_default_prefix(target):
+        return
+    print('The image you\'re trying to set as default does not exist.')
+    raise FileNotFoundError
+
+  def delete_image(self, target):
+    filenames = os.listdir(self.images_dir)
+    for filename in filenames:
+      if filename == target:
+        full_path = os.path.join(self.images_dir, filename)
+        os.remove(full_path)
 
   @property
   def ffs_model_id(self):
